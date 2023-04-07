8c47f27b894e7bd10353c848d6638b981581e3d5
tadeojcocucci <tadeojcocucci@gmail.com>
2023-04-07 12:27:04 -0300

    Name in Logo

--- src/components/global-nav.tsx
+++ src/components/global-nav.tsx
@@ -25,7 +25,7 @@ export function GlobalNav() {
           </div>
 
           <h3 className="font-semibold tracking-wide text-gray-400 group-hover:text-gray-50">
-            App Router <span className="Work in progress">(WIP)</span>
+            Tadeo Javier Cocucci
           </h3>
         </Link>
       </div>
