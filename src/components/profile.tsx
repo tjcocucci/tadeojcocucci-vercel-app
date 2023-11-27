b0b85addd200a7007d2191c03abc938d85f5939d
Tadeo <tadeojcocucci@gmail.com>
2023-11-27 15:43:51 -0300

    updated profile card

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import profilePic from "../../public/profile.jpg";
 import { Card } from "./card";
+import MailIcon from "public/mail";
+import GitHubIcon from "public/github";
+import LinkedInIcon from "public/linkedin";
+import { LocalizedText as t } from "@/context/languageContext";
 
 export function Profile({ width }: { width?: number }) {
   return (
@@ -20,8 +24,24 @@ export function ProfileCard() {
     <Card>
       <div className="flex items-center w-150 mr-4">
         <Profile />
+        <MailIcon />
       </div>
-      <div className="w-full callout">Tadeo Javier Cocucci</div>
+      <div className="block">
+        <p className="w-full callout text-lg mb-0">Tadeo Javier Cocucci</p>
+        <p className="w-full callout mb-8 mt-0">{t("computerSciencesphd")}</p>
+        <p className="flex items-center space-x-2 mb">
+          <MailIcon color="white" />
+          <a href="mailto:tadeojcocucci@gmail.com">tadeojcocucci@gmail.com</a>
+        </p>
+        <p className="flex items-center space-x-2">
+          <GitHubIcon color="white" />
+          <a href="github.com/tjcocucci">github.com/tjcocucci</a>
+        </p>
+        <p className="flex items-center space-x-2 align-baseline">
+          <LinkedInIcon color="white" />
+          <a href="linkedin.com/in/tjcocucci/">linkedin.com/in/tjcocucci/</a>
+        </p>
+      </div>
     </Card>
   );
 }
