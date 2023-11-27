687be2db44f33541596a06c96daf6ca8c42587e0
Tadeo <tadeojcocucci@gmail.com>
2023-11-27 13:42:44 -0300

    removed some unused code

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -16,49 +16,24 @@ export function Menu() {
 
   return (
     <div className="p-4 w-1/3 ">
-      {/* Menu button when screen too narrow */}
-      <button
-        type="button"
-        className="group absolute right-0 top-0 flex h-14 items-center gap-x-2 px-4 lg:hidden"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className="font-medium text-gray-100 group-hover:text-gray-400">
-          Menu
-        </div>
-        {/* {isOpen ? (
-          <XIcon className="block w-6 text-gray-400" />
-        ) : (
-          <MenuAlt2Icon className="block w-6 text-gray-400" />
-        )} */}
-      </button>
-      <div className="div"> {t("aboutMe")}</div>
-
-      <div
-        className={clsx("overflow-y-auto lg:static lg:block", {
-          "fixed inset-x-0 bottom-0 top-14 mt-px bg-white dark:bg-black":
-            isOpen,
-          hidden: !isOpen,
-        })}
-      >
-        {/* Menu items */}
-        <nav className="space-y-6 px-2 py-5">
-          {contentItems.map((section) => {
-            return (
-              <div key={section.name}>
-                <div className="mb-2 px-3 text-xs font-semibold uppercase tracking-wider text-gray-400/80">
-                  <div>{t(section.name)}</div>{" "}
-                </div>
-
-                <div className="space-y-1">
-                  {section.items.map((item) => (
-                    <GlobalNavItem key={item.slug} item={item} close={close} />
-                  ))}
-                </div>
+      {/* Menu items */}
+      <nav className="space-y-6 px-2 py-5">
+        {contentItems.map((section) => {
+          return (
+            <div key={section.name}>
+              <div className="mb-2 px-3 text-xs font-semibold uppercase tracking-wider text-gray-400/80">
+                <div>{t(section.name)}</div>{" "}
               </div>
-            );
-          })}
-        </nav>
-      </div>
+
+              <div className="space-y-1">
+                {section.items.map((item) => (
+                  <GlobalNavItem key={item.slug} item={item} close={close} />
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </nav>
     </div>
   );
 }
