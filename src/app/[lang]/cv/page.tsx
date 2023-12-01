c0cedc8f0b3d55cd521d55a2995cc2844be28aec
Tadeo <tadeojcocucci@gmail.com>
2023-12-01 10:58:42 -0300

    i think almost finished with light/dark styling

--- src/app/[lang]/cv/page.tsx
+++ src/app/[lang]/cv/page.tsx
@@ -5,6 +5,7 @@ import { allCards } from "contentlayer/generated";
 import { format, parseISO } from "date-fns";
 import { Mdx } from "@/components/mdx";
 import { useLocale } from "@/context/language-context";
+import { LocalizedText as t } from "@/context/language-context";
 
 export default function Page() {
   const locale = useLocale();
@@ -17,10 +18,12 @@ export default function Page() {
   let date = cv.date || "Unknown";
   return (
     <div className="space-y-4">
-      <h1 className="text-xl font-medium text-white-400/80">{cv.title}</h1>
-      <p className="text-xl font-small text-gray-400/80">
-        Última actualización {format(parseISO(date), "d MMM, y")}
+      <h1 className="text-2xl font-medium dark:text-gray-200 text-gray-800">{cv.title}</h1>
+      <p className="text-xs text-gray-400/80">
+        {t("lastUpdate")} {format(parseISO(date), "d MMM, y")}
       </p>
+      <hr className="border-gray-700 dark:border-gray-200" />
+      <br />
       <Mdx code={cv.body.code} />
     </div>
   );
