d182f26713d38469a0bcd9ca99f15b0386b6a665
Tadeo <tadeojcocucci@gmail.com>
2023-12-04 17:38:44 -0300

    removed pipe | between languages

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -17,24 +17,20 @@ export default function LocaleSwitcher() {
   return (
     <div className="flex flex-row space-x-2">
       {Object.entries(i18n.locales).map(([id, name], index) => (
-        <>
-          <button
-            className={clsx(
-              "rounded-md px-3 py-2 text-sm font-medium hover:bg-neutral-300 text-gray-800 hover:dark:bg-gray-800 dark:text-gray-200 fill-gray-800 dark:fill-gray-200",
-              {
-                "text-gray-400 hover:bg-gray-800": name !== userLanguage,
-                "border-2 dark:border-gray-200 border-neutral-400": name === userLanguage,
-              },
-            )}
-            key={name}
-            onClick={() => handleLanguageChange(name)}
-          >
-            {name}
-          </button>
-          <span key={id} className="text-gray-400 py-2 font-extrabold">
-            {index !== i18n.locales.length - 1 ? " | " : ""}
-          </span>
-        </>
+        <button
+          className={clsx(
+            "rounded-md px-3 py-2 text-sm font-medium hover:bg-neutral-300 text-gray-800 hover:dark:bg-gray-800 dark:text-gray-200 fill-gray-800 dark:fill-gray-200",
+            {
+              "text-gray-400 hover:bg-gray-800": name !== userLanguage,
+              "border-2 dark:border-gray-200 border-neutral-400":
+                name === userLanguage,
+            },
+          )}
+          key={"button-" + name}
+          onClick={() => handleLanguageChange(name)}
+        >
+          {name}
+        </button>
       ))}
     </div>
   );
