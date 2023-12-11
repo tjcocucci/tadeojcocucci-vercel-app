8dd3df0563f9d20e7d9f4402b2fdb69957399cfe
Tadeo <tadeojcocucci@gmail.com>
2023-12-11 09:46:03 -0300

    kebabcase to camelcase

--- public/svg-icons/linux.tsx
+++ public/svg-icons/linux.tsx
@@ -4,8 +4,8 @@ export default function LinuxIcon({ width = 24, height = 24 }) {
       <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         <path
